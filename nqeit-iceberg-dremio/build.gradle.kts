023263dc7f599244faa3e6267886c19ad22651e6
sagar-agarwal2404 <sagar.agarwal@dremio.com>
2022-10-19 17:38:05 +0530

    build file changes

--- nqeit-iceberg-dremio/build.gradle.kts
+++ nqeit-iceberg-dremio/build.gradle.kts
@@ -37,10 +37,6 @@ dependencies {
 
   testImplementation("io.rest-assured:rest-assured:$versionRestAsssured")
   testImplementation("io.rest-assured:json-schema-validator:$versionRestAsssured")
-  testImplementation("io.rest-assured:kotlin-extensions:5.2.0")
-
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
   compileOnly(libs.findbugs.jsr305)
 
